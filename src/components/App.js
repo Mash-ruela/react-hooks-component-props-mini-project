d50dac139223fc870ee7b2551f07918cd9564d52
Mash-Ruela <ruelamacharia2007@gmail.com>
2025-04-26 11:16:00 +0300

    lab

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,21 @@ import blogData from "../data/blog";
 import Header from "./Header";
 import About from "./About";
 import ArticleList from "./ArticleList";
-// console.log(blogData);
 
-const posts = blogData.posts;
-// console.log(post);
+console.log(blogData);
 
 function App() {
+
   return (
     <div className="App">
-      <Header name={blogData.name} />
-      <About image={blogData.image} about={blogData.about} alt={"blog logo"} />
-      <ArticleList posts={posts} />
+      <Header name={blogData.name}/>
+
+      <About 
+        image={blogData.image}
+        about={blogData.about} 
+      />
+
+      <ArticleList posts={blogData.posts}/>
     </div>
   );
 }
